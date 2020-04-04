e92f3c4ee5695288167d173edcfb9a52ce75cf63
Sasha <vasilieva.aleksandra@bk.ru>
2020-04-04 22:21:53 +0300

    Разбивает отрисовку страницы на модули

---
+++ src/components/extra-film-lists.js
@@ -0,0 +1,8 @@
+export const createExtraFilmsContainer = (heading) => {
+  return (
+    `<section class="films-list--extra">
+    <h2 class="films-list__title">${heading}</h2>
+      <div class="films-list__container">
+      </div>
+    </section>`);
+};
---
+++ src/tools/consts.js
@@ -0,0 +1,7 @@
+export const CARD_AMOUNT = 5;
+export const EXTRA_CARD_AMOUNT = 2;
+
+export const Position = {
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`
+};
