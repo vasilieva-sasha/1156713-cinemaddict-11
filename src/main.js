e575f43c23bdf698f3802bd5ae2a5578006880ca
Sasha <vasilieva.aleksandra@bk.ru>
2020-04-24 23:34:43 +0300

    создает контроллер и переносит в него рендеринг контейнера с фильмами

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import Sort from "./components/sorting/sort";
 import Navigation from "./components/navigation/navigation";
 import {render} from "./tools/utils/render";
 import {Position} from "./consts/consts";
-import {renderFilmContainer} from "./components/film-list/render-film-container";
 import FooterStatisticsMarkup from "./components/footer/footer-statistics";
+import PageController from "./controllers/page-controller";
+import FilmContainer from "./components/film-list/film-container";
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
@@ -15,15 +16,18 @@ const init = () => {
   const sortingComponent = new Sort();
   const navigationComponent = new Navigation();
   const statisticsComponent = new FooterStatisticsMarkup();
+  const filmContainerComponent = new FilmContainer();
 
   render(header, headerProfileComponent, Position.BEFOREEND);
   render(main, navigationComponent, Position.BEFOREEND);
   render(main, sortingComponent, Position.BEFOREEND);
 
-  renderFilmContainer();
+  render(main, filmContainerComponent, Position.BEFOREEND);
+
+  const pageController = new PageController(filmContainerComponent);
+  pageController.render();
 
   render(footer, statisticsComponent, Position.BEFOREEND);
-
 };
 
 init();
